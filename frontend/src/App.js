a413b597db3c3ddcddb508d26aceda60bc65f297
Kaveesha Nethmi <kaveesha.nethmi1@gmail.com>
2022-07-21 23:25:41 +0530

    create Ecommerce app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href ="/">GoodLuck Print House</a>
+      </header>
+      <main>list Products</main>
+    </div>
+  );
+}
+
+export default App;

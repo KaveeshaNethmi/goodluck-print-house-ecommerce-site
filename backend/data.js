7edfcfe6fb671053b5b53854254283df5901748f
Kaveesha Nethmi <kaveesha.nethmi1@gmail.com>
2022-07-27 00:31:30 +0530

    ecommerce site

---
+++ backend/data.js
@@ -0,0 +1,70 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'Kaveesha',
+      email: 'admin@example.com',
+      password:bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'Pasindu ',
+      email: 'admin@example.com',
+      password:bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      //_id: '1',
+      name: 'Bill Books',
+      slug: 'bill-books',
+      category: 'Offset Printing',
+      image: '/images/p1.jpg',
+      price: 200,
+      type: 'With Carbon Paper',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality',
+    },
+    {
+      //_id: '2',
+      name: 'Posters',
+      slug: 'posters',
+      category: 'Digital Printing',
+      image: '/images/p2.jpg',
+      price: 300,
+      type: 'One Color',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality',
+    },
+    {
+      //_id: '3',
+      name: 'Mug Printing',
+      slug: 'mug-printing',
+      category: '',
+      image: '/images/p3.jpg',
+      price: 500,
+      type: '',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality',
+    },
+    {
+      //_id: '4',
+      name: 'Invitation Cards',
+      slug: 'invitation-cards',
+      category: 'Invitation Cards',
+      image: '/images/p4.jpg',
+      price: 200,
+      type: 'Wedding Invitation Cards',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High Quality',
+    },
+  ],
+};
+
+export default data;
